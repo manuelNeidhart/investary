c496e94834d569a3f433992cc37e16c15a36a307
AHITM13 <AHITM13@leomac03.local>
2022-05-20 01:07:08 +0200

    added tasks

--- investary/CourseViewController.swift
+++ investary/CourseViewController.swift
@@ -6,18 +6,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CourseViewController: UIViewController {
 
+    var utterance : AVSpeechUtterance!
+    
+    @IBOutlet weak var wordNameLabel: UILabel!
+    @IBOutlet weak var courseText: UITextView!
+    @IBAction func listenButton(_ sender: Any) {
+    }
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        let line = CAShapeLayer()
-         let linePath = UIBezierPath()
-         linePath.move(to: CGPoint(x: 130, y: 205))
-         linePath.addLine(to: CGPoint(x: 285, y: 205))
-         line.path = linePath.cgPath
-         line.strokeColor = UIColor.black.cgColor
-         self.view.layer.addSublayer(line)
+        parseJSON() { (courses) -> () in
+            print(courses.courseElements!)
+            self.courseText.text = courses.courseElements![0].description?.description
+            self.wordNameLabel.text = courses.courseElements![0].wordName?.description
+            self.utterance = AVSpeechUtterance(string: courses.courseElements![0].description?.description ?? "nix da")
+        }
+        
+   
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        utterance.rate = 0.1
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+    
+    func parseJSON(completionHandler: @escaping ((_ courses: course)->())) {
+        guard let path = Bundle.main.path(forResource: "./json/course", ofType: "json") else {
+            
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        
+        var result: course?
+        do {
+            let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(course.self, from: jsonData)
+            print(jsonData)
+            print(result)
+            if let result = result {
+                print(result)
+                completionHandler(result)
+            }
+            else {
+                print("Failed to parse")
+            }
+        }
+        catch {
+            print("Error: \(error)")
+        }
+        
     }
 }
+
+struct course: Decodable {
+    let courseName: String?
+    let courseId: Int?
+
+    struct courseElements: Decodable {
+        let wordName: String?
+        let wordId: Int?
+        let isWordFinished: Bool?
+        let description: String?
+    }
+    let courseElements: [courseElements]?
+}
+
+
---
+++ investary/services/courseService.swift
@@ -0,0 +1,8 @@
+//
+//  courseService.swift
+//  investary
+//
+//  Created by AHITM13 on 19.05.22.
+//
+
+import Foundation
