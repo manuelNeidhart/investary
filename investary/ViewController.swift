64c6d1c824359662485bf07181423a90898ffaf0
AHITM16 <AHITM16@LeoMac06.local>
2022-06-24 02:33:48 +0200

    F..des woa nix..alarm stufe hoib 3

--- investary/ViewController.swift
+++ investary/ViewController.swift
@@ -11,10 +11,37 @@ class ViewController: UIViewController {
     @IBAction func firstSegueButton(_ sender: Any) {
     }
     
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
             super.viewDidLoad()
-            self.navigationItem.hidesBackButton = true
+        //tableView.delegate = self
+        //tableView.dataSource = self
+        
+            
         }
+}
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me")
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        cell.textLabel?.text = "Hello World"
+        
+        return cell
+    }
 }
     
 
